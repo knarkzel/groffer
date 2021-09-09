79d22fe72fc53e5ce524b2cc6dd0d074cca23ff1
Odd-Harald Lillestø Myhren <ohmyhren@uia.no>
2021-09-09 04:14:38 +0200

    add structopt, not_in

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_until},
+    bytes::complete::{is_not, tag, take_until},
     multi::{many0, many1},
     sequence::tuple,
     IResult,
@@ -24,12 +24,13 @@ fn header(input: &str) -> IResult<&str, Markdown> {
 }
 
 fn paragraph(input: &str) -> IResult<&str, Markdown> {
-    let mut parser = tuple((tag("\n\n"), take_until("\n\n"), tag("\n\n")));
+    let mut parser = tuple((tag("\n\n"), is_not("\n\n"), tag("\n\n")));
     let (input, (_, body, _)) = parser(input)?;
     Ok((input, Markdown::Paragraph { body }))
 }
 
 fn main() {
+    color_backtrace::install();
     let input = include_str!("../examples/basic.md");
     let trimmed = format!("{}\n", input);
     let output = parse(&trimmed).unwrap();
